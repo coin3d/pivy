4c6f8ad29a63a75c35beb7eeff72ff50b21c405e
Tamer Fahmy <tamer@sim.no>
2002-03-15 07:00:00 +0000

    I couldn't stop and pivied another 2 examples. it works so seamlessly that I am starting to get frightened... *huh*

---
+++ examples/Mentor/python/02.3.Trackball.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+###
+# Copyright (c) 2002, Tamer Fahmy <tamer@tammura.at>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#   * Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+#   * Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in
+#     the documentation and/or other materials provided with the
+#     distribution.
+#   * Neither the name of the copyright holder nor the names of its
+#     contributors may be used to endorse or promote products derived
+#     from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+###
+# This is an example from the Inventor Mentor,
+# chapter 2, example 3.
+#
+# Use the trackball manipulator to edit/rotate a red cone
+#
+
+from pivy import *
+import sys
+
+def main():
+	# Initialize Inventor and Gtk
+	myWindow = SoGtk_init(sys.argv[0])
+	if myWindow == None: sys.exit(1)
+
+	root = SoSeparator()
+	root.ref()
+
+	myCamera = SoPerspectiveCamera()
+	root.addChild(myCamera)             # child 0
+	root.addChild(SoDirectionalLight()) # child 1
+	root.addChild(SoTrackballManip())   # child 2
+
+	myMaterial = SoMaterial()
+	myMaterial.diffuseColor.setValue(1.0, 0.0, 0.0)
+	root.addChild(myMaterial)
+	root.addChild(SoCone())
+
+	myRenderArea = SoGtkRenderArea(myWindow)
+	myCamera.viewAll(root, myRenderArea.getViewportRegion())
+	myRenderArea.setSceneGraph(root)
+	myRenderArea.setTitle("Trackball")
+	myRenderArea.show()
+
+	SoGtk_show(myWindow)
+	SoGtk_mainLoop()
+
+if __name__ == "__main__":
+	main()

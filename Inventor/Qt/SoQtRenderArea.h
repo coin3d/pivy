d794fdff97c90434a658d0cf77f42b082e31cb93
Tamer Fahmy <tamer@sim.no>
2003-07-03 04:27:12 +0000

    the first brick for the pyqt bridge is deployed. from the comments:
    the next stunt here deserves documentation as otherwise i would not
    know what is going on here by tomorrow morning!
    
    What are we doing here? hacking in extremo! i had to find a way to pass
    the QEvent instance we get to PyQt!
    
    the approach i chose is to create a QEvent instance in Python from PyQt
    (obviously to let this work the user had to import PyQt beforehand. he
    has to otherwise he has no reason to create this callback).
    this gives us a fully and properly instantiated structure as PyQt
    expects it without digging into sip or depending on the sip library.
    
    then i pass the instantiated structure over here and grab the sipThis
    entry from the instance which holds a u.cppPtr which turns
    out to be the real Qt Object in memory. *har, we are in business now*
    
    now i delete the current pointer and let u.cppPtr point to our very own
    QEvent Object. amazingly enough this really works...

--- Inventor/Qt/SoQtRenderArea.h
+++ Inventor/Qt/SoQtRenderArea.h
@@ -49,14 +49,137 @@ typedef SbBool SoQtRenderAreaEventCB(void * closure, QEvent * event);
 
 #ifdef __PIVY__
 %{
+/* sip specific stuff that allows to bridge to PyQt */
+/**
+ * as this solution might appeal to the devil himself - here an attempt
+ * of clarification:
+ *
+ * You: but tamer this is a really really ugly hack! shame on you!
+ *  Me: guilty! *blush* ey, but what else can I do, huh?
+ *      so would you plz. just join my prayers that the sip structures are 
+ *      not going to change any time soon as otherwise we are going to burn
+ *      in _HELL_ and won't see any angels any time soon!
+ */
+
+/*
+ * A Python method's component parts.  This allows us to re-create the method
+ * without changing the reference counts of the components.
+ */
+
+typedef struct {
+	PyObject *mfunc;		/* The function. */
+	PyObject *mself;		/* Self if it is a bound method. */
+	PyObject *mclass;		/* The class. */
+} sipPyMethod;
+
+/*
+ * Extra type specific information.
+ */
+typedef struct {
+	const void *(*castfunc)(const void *,PyObject *);	/* Cast function. */
+	void *proxyfunc;		/* Create proxy function. */
+	struct _sipQtSignal *emitTable;	/* Emit table for Qt sigs (complex). */
+} sipExtraType;
+
+/*
+ * A slot.
+ */
+
+typedef struct {
+	char *name;			/* Name if a Qt or Python signal. */
+	PyObject *pyobj;		/* Signal or Qt slot object. */
+	sipPyMethod meth;		/* Python slot method, pyobj is NULL. */
+	PyObject *weakSlot;		/* A weak reference to the slot. */
+} sipSlot;
+
+/*
+ * A receiver of a Python signal.
+ */
+
+typedef struct _sipPySigRx {
+	sipSlot rx;			/* The receiver. */
+	struct _sipPySigRx *next;	/* Next in the list. */
+} sipPySigRx;
+
+/*
+ * A Python signal.
+ */
+
+typedef struct _sipPySig {
+	char *name;			/* The name of the signal. */
+	sipPySigRx *rxlist;		/* The list of receivers. */
+	struct _sipPySig *next;		/* Next in the list. */
+} sipPySig;
+
+/*
+ * A C/C++ object wrapped as a Python object.
+ */
+
+typedef struct _sipThisType {
+	PyObject_HEAD
+	union {
+		const void *cppPtr;	/* C/C++ object pointer. */
+		const void *(*afPtr)();	/* Access function. */
+	} u;
+	int flags;			/* Object flags. */
+	PyObject *sipSelf;		/* The Python class instance. */
+	sipPySig *pySigList;		/* Python signal list (complex). */
+	sipExtraType *xType;		/* Extra type information. */
+} sipThisType;
+
+/*
+ * Maps the name of a Qt signal to a wrapper function to emit it.
+ */
+
+typedef struct _sipQtSignal {
+	char *st_name;
+	int (*st_emitfunc)(sipThisType *,PyObject *);
+} sipQtSignal;
+
+
 static SbBool
 SoQtRenderAreaEventPythonCB(void * closure, QEvent * event)
 {
   PyObject *func, *arglist;
   PyObject *result, *qev;
+  sipThisType *sipThis;
   int ires = 0;
 
-  qev = SWIG_NewPointerObj((void *) event, SWIGTYPE_p_QEvent, 1);
+  /** 
+   * the next stunt here deserves documentation as otherwise i would not
+   * know what is going on here by tomorrow morning!
+   *
+   * What are we doing here? hacking in extremo! i had to find a way to pass
+   * the QEvent instance we get to PyQt!
+   *
+   * the approach i chose is to create a QEvent instance in Python from PyQt
+   * (obviously to let this work the user had to import PyQt beforehand. he
+   * has to otherwise he has no reason to create this callback).
+   * this gives us a fully and properly instantiated structure as PyQt 
+   * expects it without digging into sip or depending on the sip library.
+   *
+   * then i pass the instantiated structure over here and grab the sipThis
+   * entry from the instance which holds a u.cppPtr which turns
+   * out to be the real Qt Object in memory. *har, we are in business now*
+   *
+   * now i delete the current pointer and let u.cppPtr point to our very own
+   * QEvent Object. amazingly enough this really works...
+   */
+
+  /* FIXME: is it necessary to pass the real event->type instead of
+   *   defaulting to QEvent.None? seems to work as is but if yes then
+   *   snprintf and some strlen/malloc/memset/free magic are your
+   *   friends. 20030703 tamer.
+   */
+  PyRun_SimpleString("qev = QEvent(QEvent.None)");
+
+  PyObject *d = PyModule_GetDict(PyImport_AddModule("__main__"));
+  qev = PyRun_String("qev", Py_eval_input, d, d);
+  
+  sipThis = (sipThisType *)PyDict_GetItem(((PyInstanceObject *)qev)->in_dict,
+                                          PyString_FromString("sipThis"));
+  delete (QEvent *)sipThis->u.cppPtr;
+  sipThis->u.cppPtr = event;
 
   /* the first item in the closure sequence is the python callback
    * function; the second is the supplied closure python object */

8fcfe4bcb18e63fafa0b13513846c249b33dba19
Tamer Fahmy <tamer@sim.no>
2008-05-08 10:17:42 +0000

    use QApplication instead of QuarterApplication and initialize SensorManager and ImageReader in QuarterWidget. Coin initialization in the Quarter C++ class is taken care of implicitly in Pivy already.

--- pivy/quarter/QuarterApplication.py
+++
@@ -1,9 +0,0 @@
-from PyQt4.QtGui import QApplication
-
-from Quarter import Quarter
-
-class QuarterApplication(QApplication):
-    def __init__(self, args):
-        QApplication.__init__(self, args)
-
-        self.quarter = Quarter()
--- pivy/quarter/QuarterWidget.py
+++ pivy/quarter/QuarterWidget.py
@@ -1,3 +1,115 @@
+###
+# Copyright (c) 2002-2008 Kongsberg SIM
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+
+"""
+  Quarter is a light-weight glue library that provides seamless
+  integration between Systems in Motions's \COIN high-level 3D
+  visualization library and Trolltech's \QT 2D user interface
+  library.
+
+  \QT and \COIN is a perfect match since they are both open source,
+  widely portable and easy to use. Quarter has evolved from Systems in
+  Motion's own experiences using \COIN and \QT together in our
+  applications.
+
+  The functionality in Quarter revolves around QuarterWidget, a
+  subclass of QGLWidget. This widget provides functionality for
+  rendering of Coin scenegraphs and translation of QEvents into
+  SoEvents. Using this widget is as easy as using any other QWidget.
+
+  \subpage QuarterWidgetPlugin
+
+  Quarter also comes with a plugin for Qt Designer, Trolltech's tool
+  for designing and building GUIs. Once you install Quarter, the
+  QuarterWidget becomes accessible in Qt Designer, and you can include
+  it in the user interfaces you create. The plugin facility also
+  provides you with the capability of dynamically loading ui files
+  containing a QuarterWidget in your application.
+
+  By using \COIN, \QT and Quarter to build your 3D graphics
+  applications, you have the power to write software that is portable
+  across the whole range of UNIX, Linux, Microsoft Windows and Mac OS
+  X operating systems, from a 100% common codebase.
+
+  For a small, completely stand-alone usage example on how to
+  initialize the library and set up a viewer instance window, see the
+  following code:
+
+  \code
+  #include <Inventor/nodes/SoBaseColor.h>
+  #include <Inventor/nodes/SoCone.h>
+  #include <Inventor/nodes/SoSeparator.h>
+
+  #include <Quarter/QuarterWidget.h>
+  #include <Quarter/QuarterApplication.h>
+
+  using namespace SIM::Coin3D::Quarter;
+
+  int
+  main(int argc, char ** argv)
+  {
+    // Initializes SoQt library (and implicitly also the Coin and Qt
+    // libraries).
+    QuarterApplication app(argc, argv);
+
+    // Make a dead simple scene graph by using the Coin library, only
+    // containing a single yellow cone under the scenegraph root.
+    SoSeparator * root = new SoSeparator;
+    root->ref();
+
+    SoBaseColor * col = new SoBaseColor;
+    col->rgb = SbColor(1, 1, 0);
+    root->addChild(col);
+
+    root->addChild(new SoCone);
+
+    // Create a QuarterWidget for displaying a Coin scene graph
+    QuarterWidget * viewer = new QuarterWidget;
+    viewer->setSceneGraph(root);
+
+    // Pop up the QuarterWidget
+    viewer->show();
+    // Loop until exit.
+    app.exec();
+    // Clean up resources.
+    root->unref();
+    delete viewer;
+
+    return 0;
+  }
+  \endcode
+
+  \subpage examples
+
+  \page examples More Examples
+
+  The examples code is included in Quarter and can be found in the
+  src/examples subdirectory.
+
+  \subpage directui
+
+  \subpage dynamicui
+
+  \subpage inheritui
+
+  \subpage mdi
+
+  \subpage examiner
+"""
+
 from PyQt4.QtOpenGL import QGLWidget
 from PyQt4.QtOpenGL import QGLFormat
 from PyQt4.QtOpenGL import QGL
@@ -26,6 +138,8 @@ from devices import KeyboardHandler
 
 from eventhandlers import EventManager
 
+from SensorManager import SensorManager
+from ImageReader import ImageReader
 
 # FIXME jkg: (1) this is not called and (2) change to private/static method?
 def renderCB(closure, rendermanagerdummy):
@@ -71,8 +185,21 @@ def postrendercb(userdata, manager):
 
 
 class QuarterWidget(QGLWidget):
+
+    _sensormanager = None
+    _imagereader = None
+
     def __init__(self, parent = None, sharewidget = None):
         QGLWidget.__init__(self, parent)
+
+        # initialize Sensormanager and ImageReader instances only once
+        if not QuarterWidget._sensormanager:
+            QuarterWidget._sensormanager = SensorManager()
+
+        # FIXME: enable once ImageReader has been translated. 20080508
+        # tamer.
+        if 0 and not QuarterWidget._imagereader:
+            QuarterWidget._imagereader = ImageReader()
 
         # from QuarterWidgetP
         self.cachecontext_list = []

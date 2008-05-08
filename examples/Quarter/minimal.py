2b80cf410f0ce7a99a0c6b89a9cdf6780073e3af
Tamer Fahmy <tamer@sim.no>
2008-05-08 09:31:09 +0000

    small cleanup.

--- examples/Quarter/minimal.py
+++ examples/Quarter/minimal.py
@@ -1,3 +1,21 @@
+#!/usr/bin/env python
+
+###
+# Copyright (c) 2002-2008 Kongsberg SIM
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+
 import sys
 
 from pivy.coin import SoSeparator
@@ -8,13 +26,10 @@ from pivy.coin import SoCone
 from pivy.quarter import QuarterApplication
 from pivy.quarter import QuarterWidget
 
-
 def main():
-
     app = QuarterApplication(sys.argv)
 
     root = SoSeparator()
-    root.ref()
     col = SoBaseColor()
     col.rgb = SbColor(1, 1, 0)
     root.addChild(col)
@@ -27,4 +42,5 @@ def main():
     result = app.exec_()
     return result
 
-main()
+if __name__ == '__main__':
+    main()

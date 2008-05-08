2677caac1cc6ae779f805286ff308ecdc03c6ea4
Tamer Fahmy <tamer@sim.no>
2008-05-08 15:35:09 +0000

    fix up SoEvent/QEvent handling.

--- pivy/quarter/devices/DeviceHandler.py
+++ pivy/quarter/devices/DeviceHandler.py
@@ -1,27 +1,24 @@
-from pivy.coin import SbTime
+from PyQt4 import QtCore
 
+from pivy import coin
 
 class DeviceHandler:
     """
     The DeviceHandler class is the base class for eventhandlers
-  such as the KeyboardHandler and MouseHandler. It can be subclassed
-  to create event handlers for other devices.
-"""
-    def __init__(self):
-        pass
-
+    such as the KeyboardHandler and MouseHandler. It can be subclassed
+    to create event handlers for other devices.
+    """
+    
     def setManager(self, manager):
         self.manager = manager
 
     def setModifiers(self, soevent, qevent):
         # FIXME: How do we get the time from the qevent? (20070306 frodo)
-        soevent.setTime(SbTime.getTimeOfDay())
+        soevent.setTime(coin.SbTime.getTimeOfDay())
 
         # Note: On Mac OS X, the ControlModifier value corresponds to the
         # Command keys on the Macintosh keyboard, and the MetaModifier
         # value corresponds to the Control keys.
-        #print "FIXME jkg: setmodifiers"
-#        print soevent
-#        soevent.setShiftDown(qevent.modifiers() and Qt.ShiftModifier)
-#        soevent.setAltDown(qevent.modifiers() and Qt.AltModifier)
-#        soevent.setCtrlDown(qevent.modifiers() and Qt.ControlModifier)
+        soevent.setShiftDown(int(qevent.modifiers() & QtCore.Qt.ShiftModifier) == QtCore.Qt.ShiftModifier)    
+        soevent.setAltDown(int(qevent.modifiers() & QtCore.Qt.AltModifier) == QtCore.Qt.AltModifier)
+        soevent.setCtrlDown(int(qevent.modifiers() & QtCore.Qt.ControlModifier) == QtCore.Qt.ControlModifier)

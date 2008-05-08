de07b7a7d4793501a73fb2ac6b13768dd2254e0d
Tamer Fahmy <tamer@sim.no>
2008-05-08 09:25:53 +0000

    synchronized with C++ version after peer review.

--- pivy/quarter/Quarter.py
+++ pivy/quarter/Quarter.py
@@ -1,19 +1,119 @@
+###
+# Copyright (c) 2002-2008 Kongsberg SIM
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+
+"""
+  Quarter is a light-weight glue library that provides seamless
+  integration between Systems in Motions's \COIN high-level 3D
+  visualization library and Trolltech's \QT 2D user interface
+  library.
+
+  \QT and \COIN is a perfect match since they are both open source,
+  widely portable and easy to use. Quarter has evolved from Systems in
+  Motion's own experiences using \COIN and \QT together in our
+  applications.
+
+  The functionality in Quarter revolves around QuarterWidget, a
+  subclass of QGLWidget. This widget provides functionality for
+  rendering of Coin scenegraphs and translation of QEvents into
+  SoEvents. Using this widget is as easy as using any other QWidget.
+
+  \subpage QuarterWidgetPlugin
+
+  Quarter also comes with a plugin for Qt Designer, Trolltech's tool
+  for designing and building GUIs. Once you install Quarter, the
+  QuarterWidget becomes accessible in Qt Designer, and you can include
+  it in the user interfaces you create. The plugin facility also
+  provides you with the capability of dynamically loading ui files
+  containing a QuarterWidget in your application.
+
+  By using \COIN, \QT and Quarter to build your 3D graphics
+  applications, you have the power to write software that is portable
+  across the whole range of UNIX, Linux, Microsoft Windows and Mac OS
+  X operating systems, from a 100% common codebase.
+
+  For a small, completely stand-alone usage example on how to
+  initialize the library and set up a viewer instance window, see the
+  following code:
+
+  \code
+  #include <Inventor/nodes/SoBaseColor.h>
+  #include <Inventor/nodes/SoCone.h>
+  #include <Inventor/nodes/SoSeparator.h>
+
+  #include <Quarter/QuarterWidget.h>
+  #include <Quarter/QuarterApplication.h>
+
+  using namespace SIM::Coin3D::Quarter;
+
+  int
+  main(int argc, char ** argv)
+  {
+    // Initializes SoQt library (and implicitly also the Coin and Qt
+    // libraries).
+    QuarterApplication app(argc, argv);
+
+    // Make a dead simple scene graph by using the Coin library, only
+    // containing a single yellow cone under the scenegraph root.
+    SoSeparator * root = new SoSeparator;
+    root->ref();
+
+    SoBaseColor * col = new SoBaseColor;
+    col->rgb = SbColor(1, 1, 0);
+    root->addChild(col);
+
+    root->addChild(new SoCone);
+
+    // Create a QuarterWidget for displaying a Coin scene graph
+    QuarterWidget * viewer = new QuarterWidget;
+    viewer->setSceneGraph(root);
+
+    // Pop up the QuarterWidget
+    viewer->show();
+    // Loop until exit.
+    app.exec();
+    // Clean up resources.
+    root->unref();
+    delete viewer;
+
+    return 0;
+  }
+  \endcode
+
+  \subpage examples
+
+  \page examples More Examples
+
+  The examples code is included in Quarter and can be found in the
+  src/examples subdirectory.
+
+  \subpage directui
+
+  \subpage dynamicui
+
+  \subpage inheritui
+
+  \subpage mdi
+
+  \subpage examiner
+"""
+
 from pivy import coin
 
 from SensorManager import SensorManager
 
-initialized = False
-
-
 class Quarter:
     def __init__(self):
-        global initialized
-        assert(not initialized)
-        coin.SoDB.init()
-        coin.SoNodeKit.init()
-        # FIXME jkg: do this?
-        #coin.SoBaseKit.setSearchingChildren(coin.TRUE) # we always want to search under node kits
-        # FIXME jkg: waiting for wrapper?
-        #coin.SoInteractoin.init()
-
-        self.sensormanager = SensorManager()
+        self._sensormanager = SensorManager()
--- pivy/quarter/SensorManager.py
+++ pivy/quarter/SensorManager.py
@@ -1,3 +1,19 @@
+###
+# Copyright (c) 2002-2008 Kongsberg SIM
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+
 from PyQt4.QtCore import QTimer
 from PyQt4.QtCore import QObject
 from PyQt4.QtCore import QThread
@@ -10,118 +26,94 @@ from pivy.coin import SoRenderManager
 
 from SignalThread import SignalThread
 
-
 class SensorManager(QObject):
+
     def __init__(self):
+        self._mainthreadid = QThread.currentThreadId()
 
-        self.mainthreadid = QThread.currentThreadId()
+        self._signalthread = SignalThread()
+        QObject.connect(self.signalthread, SIGNAL("triggerSignal()"),
+                        self.sensorQueueChanged)
 
-        self.signalthread = SignalThread()
-        QObject.connect(self.signalthread, SIGNAL("triggerSignal()"), self.sensorQueueChanged)
+        self._idletimer = QTimer()
+        self._delaytimer = QTimer()
+        self._timerqueuetimer = QTimer()
 
-        self.idletimer = QTimer()
-        self.delaytimer = QTimer()
-        self.timerqueuetimer = QTimer()
+        self._idletimer.setSingleShot(True)
+        self._delaytimer.setSingleShot(True)
+        self._timerqueuetimer.setSingleShot(True)
 
-        self.idletimer.setSingleShot(True)
-        self.delaytimer.setSingleShot(True)
-        self.timerqueuetimer.setSingleShot(True)
-
-        self.connect(self.idletimer, SIGNAL("timeout()"), self.idleTimeout)
-        self.connect(self.delaytimer, SIGNAL("timeout()"), self.delayTimeout)
-        self.connect(self.timerqueuetimer, SIGNAL("timeout()"), self.timerQueueTimeout)
+        self.connect(self._idletimer, SIGNAL("timeout()"), self.idleTimeout)
+        self.connect(self._delaytimer, SIGNAL("timeout()"), self.delayTimeout)
+        self.connect(self._timerqueuetimer, SIGNAL("timeout()"), self.timerQueueTimeout)
 
         SoDB.getSensorManager().setChangedCallback(self.sensorQueueChangedCB, self)
         SoDB.setRealTimeInterval(1.0 / 25.0)
         SoRenderManager.enableRealTimeUpdate(False)
 
+    def __del__(self):
+        SoDB.getSensorManager().setChangedCallback(None, None)
+
+        if self._signalthread.isRunning():
+            self._signalthread.stopThread()
+            self._signalthread.wait()
+
     def sensorQueueChangedCB(self, closure):
-        thisp = closure
         # if we get a callback from another thread, route the callback
         # through SignalThread so that we receive the callback in the
         # QApplication thread (needed since QTimer isn't thread safe)
-        if QThread.currentThreadId() != thisp.mainthreadid:
-            if not thisp.signalthread.isRunning():
-                thisp.signalthread.start()
-            self.signalthread.trigger()
+        if QThread.currentThreadId() != closure._mainthreadid:
+            if not closure._signalthread.isRunning():
+                closure._signalthread.start()
+            self._signalthread.trigger()
         else:
-            print "ok"
             self.sensorQueueChanged()
 
     def sensorQueueChanged(self):
-        sm = SoDB.getSensorManager()
-        # Set up timer queue timeout if necessary.
+        sensormanager = SoDB.getSensorManager()
+        assert(sensormanager)
+  
+        interval = sensormanager.isTimerSensorPending()
 
-        t = sm.isTimerSensorPending()
-        if t:
-            # FIXME 20080424 jkg: workaround for PivyExtensions/test/regression.py
-            #ie. "interval = t - SbTime.getTimeOfDay()" wont work since t is
-            #suddenly mixed up with smallchange... strange link error?
-            a = SbTime()
-            a.setValue(t.getValue())
-            interval = a - SbTime.getTimeOfDay()
+        if interval:
+            interval -= SbTime.getTimeOfDay()
 
-            # Qt v2.1.1 (at least) on MSWindows will fail to trigger the
-            # timer if the interval is < 0.0.
-            #
-            # We also want to avoid setting it to 0.0, as that has a special
-            # semantic meaning: trigger only when the application is idle and
-            # event queue is empty -- which is not what we want to do here.
-            #
-            # So we clamp it, to a small positive value:
             if interval.getValue() <= 0.0:
                 interval.setValue(1.0/5000.0)
 
-            # Change interval of timerqueuetimer when head node of the
-            # timer-sensor queue of SoSensorManager changes.
-            if not self.timerqueuetimer.isActive():
-                self.timerqueuetimer.start(interval.getMsecValue())
+            if not self._timerqueuetimer.isActive():
+                self._timerqueuetimer.start(interval.getMsecValue())
             else:
-                self.timerqueuetimer.setInterval(interval.getMsecValue())
+                self._timerqueuetimer.setInterval(interval.getMsecValue())
+        elif self._timerqueuetimer.isActive():
+            self._timerqueuetimer.stop()
 
-        # Stop timerqueuetimer if queue is completely empty.
-        elif self.timerqueuetimer.isActive():
-            self.timerqueuetimer.stop()
 
-        if sm.isDelaySensorPending():
-            self.idletimer.start(0)
-            # Start idletimer at 0 seconds in the future. -- That means it will
-            # trigger when the Qt event queue has been run through, i.e. when
-            # the application is idle.
-            if not self.idletimer.isActive():
-                self.idletimer.start(0)
-
-                t = SoDB.getDelaySensorTimeout()
-                if t != SbTime.zero():
-                    self.delaytimer.start(interval.getMsecValue())
-        else:
-            if self.idletimer.isActive():
-                self.idletimer.stop()
-            if self.delaytimer.isActive():
-                self.delaytimer.stop()
+        if sensormanager.isDelaySensorPending():
+            this.idletimer.start(0)
+    
+            if not self._delaytimer.isActive():
+                time = SoDB.getDelaySensorTimeout()
+                
+                if time != SbTime.zero():
+                    self._delaytimer.start(interval.getMsecValue())
+            else:
+                if self._idletimer.isActive():
+                    self._idletimer.stop()
+            
+                if self._delaytimer.isActive():
+                    self._delaytimer.stop()
 
     def idleTimeout(self):
         SoDB.getSensorManager().processTimerQueue()
         SoDB.getSensorManager().processDelayQueue(True)
-
-        # The change callback is _not_ called automatically from
-        # SoSensorManager after the process methods, so we need to
-        # explicitly trigger it ourselves here.
         self.sensorQueueChanged()
 
     def timerQueueTimeout(self):
         SoDB.getSensorManager().processTimerQueue()
-
-        # The change callback is _not_ called automatically from
-        # SoSensorManager after the process methods, so we need to
-        # explicitly trigger it ourselves here.
         self.sensorQueueChanged()
 
     def delayTimeout(self):
         SoDB.getSensorManager().processTimerQueue()
         SoDB.getSensorManager().processDelayQueue(False)
-
-        # The change callback is _not_ called automatically from
-        # SoSensorManager after the process methods, so we need to
-        # explicitly trigger it ourselves here.
         self.sensorQueueChanged()

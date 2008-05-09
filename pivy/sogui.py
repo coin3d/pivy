1a7d775d66d1aae45c01bd9b041cfdc621d1f50c
Tamer Fahmy <tamer@sim.no>
2008-05-09 14:04:23 +0000

    add initial SoGui Quarter wrapper.

--- pivy/sogui.py
+++ pivy/sogui.py
@@ -35,7 +35,7 @@ class SoGui_Proxy:
         self.debug = debug
 
         # if no binding has been specified check for availability of a known
-        # one in a defined order SoQt -> SoWin -> SoXt -> SoGtk
+        # one in a defined order Quarter -> SoQt -> SoWin -> SoXt -> SoGtk
         if not gui:
             try:
                 sogui = __import__('pivy.gui.soqt').gui.soqt
@@ -91,7 +91,6 @@ class SoGui_Proxy:
 
     __str__ = __repr__
 
-
 # look for user overrides in the main dictionary of the interpreter
 gui, debug = None, False
 
@@ -106,5 +105,103 @@ if sys.modules.has_key('__main__'):
     except AttributeError:
         pass
 
-# instantiate the proxy
-SoGui = SoGui_Proxy(gui, debug)
+# if no GUI has been specified try using Quarter
+if not gui:
+    try:
+        __import__('PyQt4')
+        quarter = __import__('pivy.quarter').quarter
+        gui = 'Quarter'
+
+        import PyQt4
+        import pivy
+        
+        class SoGui(object):
+            @staticmethod
+            def init(*args):
+                SoGui_Quarter_Wrapper.qApp = PyQt4.QtGui.QApplication(sys.argv)
+                return pivy.quarter.QuarterWidget()
+
+            @staticmethod
+            def mainLoop():
+                SoGui_Quarter_Wrapper.qApp.exec_()
+
+            @staticmethod
+            def show(mainwindow):
+                pass
+
+        class SoGuiViewer(object):
+            BROWSER, EDITOR = range(2)
+
+            VIEW_AS_IS, VIEW_HIDDEN_LINE, VIEW_NO_TEXTURE, \
+            VIEW_LOW_COMPLEXITY, VIEW_LINE, VIEW_POINT, VIEW_BBOX, \
+            VIEW_LOW_RES_LINE, VIEW_LOW_RES_POINT, VIEW_SAME_AS_STILL, \
+            VIEW_WIREFRAME_OVERLAY = range(11)
+
+            STILL, INTERACTIVE = range(2)
+            BUFFER_SINGLE, BUFFER_DOUBLE, BUFFER_INTERACTIVE = range(3)
+            VARIABLE_NEAR_PLANE, CONSTANT_NEAR_PLANE = range(2)
+            
+        class SoGui_Quarter_Wrapper(object):
+            """A SoGui QuarterWidget wrapper"""
+
+            def __init__(self, mainwindow):
+                self._root = None
+                self.quarterwidget = mainwindow
+
+            def getCamera(self):
+                return self.quarterwidget.searchForCamera(self._root)
+                
+            def getSize(self):
+                size = self.quarterwidget.size()
+                return pivy.coin.SbVec2s((size.width, size.height))
+
+            def getViewportRegion(self):
+                return pivy.coin.SbViewportRegion()
+
+            def redrawOnSelectionChange(self, selection):
+                # FIXME: investigate. 20080509 tamer.
+                pass
+
+            def setBackgroundColor(self, color):
+                # FIXME: investigate. 20080509 tamer.
+                pass
+
+            def setDrawStyle(self, type, style):
+                # FIXME: we happily ignore the type. investigate. 20080509 tamer.
+                rendermgr = self.quarterwidget.getSoRenderManager()
+                rendermgr.setRenderMode(style)
+
+            def setGLRenderAction(self, renderaction):
+                rendermgr = self.quarterwidget.getSoRenderManager()
+                rendermgr.setGLRenderAction(renderaction)
+                
+            def setHeadlight(self, onOff):
+                self.quarterwidget.enableHeadlight(onOff)
+
+            def setOverlaySceneGraph(self, overlay):
+                # FIXME: investigate. 20080509 tamer.
+                pass                
+                
+            def setSceneGraph(self, root):
+                self.quarterwidget.setSceneGraph(root)
+                self._root = root
+
+            def setSize(self, size):
+                self.quarterwidget.resize(size[0], size[1])
+
+            def setTitle(self, title):
+                self.quarterwidget.setWindowTitle(title)
+
+            def show(self):
+                self.quarterwidget.show()
+
+            def viewAll(self):
+                self.quarterwidget.viewAll()
+
+        SoGuiExaminerViewer = SoGui_Quarter_Wrapper
+        SoGuiRenderArea = SoGui_Quarter_Wrapper
+    except ImportError:
+        # instantiate the SoGui proxy
+        SoGui = SoGui_Proxy(gui, debug)
+else:
+    SoGui = SoGui_Proxy(gui, debug)

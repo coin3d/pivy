723ca716ecf6b1298b05ba22fb15780d361e2afa
Marius Kintel <kintel@sim.no>
2008-08-04 12:43:52 +0000

    sync with recent API changes in Coin

--- interfaces/coin_header_includes.h
+++ interfaces/coin_header_includes.h
@@ -664,21 +664,12 @@
 #if COIN_MAJOR_VERSION > 2
 #include <Inventor/SoEventManager.h>
 #include <Inventor/SoRenderManager.h>
-#include <Inventor/scxml/ScXMLAnchor.h>
-#include <Inventor/scxml/ScXMLDocument.h>
 #include <Inventor/scxml/ScXMLEvent.h>
-#include <Inventor/scxml/ScXMLFinal.h>
 #include <Inventor/scxml/ScXML.h>
-#include <Inventor/scxml/ScXMLHistory.h>
-#include <Inventor/scxml/ScXMLInitial.h>
 #include <Inventor/scxml/ScXMLInvoke.h>
 #include <Inventor/scxml/ScXMLObject.h>
-#include <Inventor/scxml/ScXMLOnEntry.h>
-#include <Inventor/scxml/ScXMLOnExit.h>
-#include <Inventor/scxml/ScXMLState.h>
 #include <Inventor/scxml/ScXMLStateMachine.h>
 #include <Inventor/scxml/ScXMLSubObject.h>
-#include <Inventor/scxml/ScXMLTransition.h>
 #include <Inventor/scxml/SoScXMLEvent.h>
 #include <Inventor/scxml/SoScXMLStateMachine.h>
 #endif

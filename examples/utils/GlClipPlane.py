66528060b0348ebd1f17dee7c686e2ce014de39f
looooo <sppedflyer@gmail.com>
2017-04-21 12:25:08 +0200

    stencil-buffer: draw cap

--- examples/utils/GlClipPlane.py
+++ examples/utils/GlClipPlane.py
@@ -12,6 +12,7 @@ class CapPlane():
         self.dist = plane.getDistanceFromOrigin()
 
     def stencilBuffer(self):
+
         eq = GLdouble_4(*self.normal, self.dist)
         glClipPlane(GL_CLIP_PLANE0, eq)
 
@@ -19,83 +20,82 @@ class CapPlane():
 
         glEnable(GL_STENCIL_TEST)
         glClear(GL_STENCIL_BUFFER_BIT)
-        glDisable(GL_DEPTH_TEST)
-        glColorMask(GL_FALSE, GL_FALSE, GL_FALSE, GL_FALSE)
 
-        glStencilFunc(GL_ALWAYS, 0, 0)
-        glStencilOp(GL_KEEP, GL_KEEP, GL_INCR)
-        glCullFace(GL_FRONT)
-        self.draw_box()
-
+        # Draw Front faces and decrease stencil-buffer
+        # where the front_face is drawn
+        glStencilFunc(GL_ALWAYS, 0, 0);
         glStencilOp(GL_KEEP, GL_KEEP, GL_DECR)
-        glCullFace(GL_BACK)
+        glCullFace(GL_FRONT)
+        glEnable(GL_CULL_FACE)
         self.draw_box()
 
-        glEnable(GL_STENCIL_TEST)
-        glClear(GL_STENCIL_BUFFER_BIT)
-        glDisable(GL_DEPTH_TEST)
+        # draw the back_face to the stencil-buffer
+        # increase the stencil buffer where the back-face is drawn
+        glCullFace(GL_BACK)
         glColorMask(GL_FALSE, GL_FALSE, GL_FALSE, GL_FALSE)
+        glStencilOp(GL_KEEP, GL_KEEP, GL_INCR)
+        self.draw_box()
 
+        # Draw a quad aligned with the stencil plane,
+        # but set the stencil test to reject pixels unless the stencil is set.
         glColorMask(GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE)
         glEnable(GL_DEPTH_TEST)
         glDisable(GL_CLIP_PLANE0)
+        glDisable(GL_CULL_FACE)
         glStencilFunc(GL_NOTEQUAL, 0, ~0)
 
-        glColor3f(0.0, 0.2, 1)
+        glColor3f(0.1, 0.2, 1)
         glBegin(GL_QUADS)
-        glVertex3fv([-1.1,-1.1, 1.1])
-        glVertex3fv([ 1.1,-1.1, 1.1])
-        glVertex3fv([ 1.1, 1.1, -1.1])
-        glVertex3fv([-1.1, 1.1, -1.1])
+        glVertex3fv([-1.1,-1.1, 0])
+        glVertex3fv([ 1.1,-1.1, 0])
+        glVertex3fv([ 1.1, 1.1, 0])
+        glVertex3fv([-1.1, 1.1, 0])
         glEnd()
 
         glDisable(GL_STENCIL_TEST)
-        glEnable(GL_CLIP_PLANE0)
-        self.draw_box()
-        glDisable(GL_CLIP_PLANE0)
+        glClear(GL_STENCIL_BUFFER_BIT)
 
     def draw_box(self):
         glDisable(GL_LIGHTING)
         glPushMatrix()
         glColor3f(0.0, 0.7, 0.0)
         glBegin(GL_QUADS)
-        glVertex3fv([-1,-1, -1])
-        glVertex3fv([-1, 1, -1])
-        glVertex3fv([ 1, 1, -1])
-        glVertex3fv([ 1,-1, -1])
-
         glVertex3fv([-1,-1, 1])
         glVertex3fv([-1, 1, 1])
         glVertex3fv([ 1, 1, 1])
         glVertex3fv([ 1,-1, 1])
 
-        glVertex3fv([1, -1,-1])
-        glVertex3fv([1, -1, 1])
-        glVertex3fv([1,  1, 1])
-        glVertex3fv([1,  1,-1])
+        glVertex3fv([-1,-1,-1])
+        glVertex3fv([ 1,-1,-1])
+        glVertex3fv([ 1, 1,-1])
+        glVertex3fv([-1, 1,-1])
 
-        glVertex3fv([-1, -1,-1])
-        glVertex3fv([-1, -1, 1])
-        glVertex3fv([-1,  1, 1])
-        glVertex3fv([-1,  1,-1])
+        glVertex3fv([ 1,-1,-1])
+        glVertex3fv([ 1,-1, 1])
+        glVertex3fv([ 1, 1, 1])
+        glVertex3fv([ 1, 1,-1])
 
-        glVertex3fv([-1, 1, -1])
-        glVertex3fv([-1, 1,  1])
-        glVertex3fv([ 1, 1,  1])
-        glVertex3fv([ 1, 1, -1])
+        glVertex3fv([-1,-1,-1])
+        glVertex3fv([-1, 1,-1])
+        glVertex3fv([-1, 1, 1])
+        glVertex3fv([-1,-1, 1])
 
-        glVertex3fv([-1, -1, -1])
-        glVertex3fv([-1, -1,  1])
-        glVertex3fv([ 1, -1,  1])
-        glVertex3fv([ 1, -1, -1])
+        glVertex3fv([-1, 1,-1])
+        glVertex3fv([ 1, 1,-1])
+        glVertex3fv([ 1, 1, 1])
+        glVertex3fv([-1, 1, 1])
+
+        glVertex3fv([-1,-1,-1])
+        glVertex3fv([-1,-1, 1])
+        glVertex3fv([ 1,-1, 1])
+        glVertex3fv([ 1,-1,-1])
+
         glEnd()
         glPopMatrix()
         glEnable(GL_LIGHTING)
 
-# Callback routine to render the floor using OpenGL
 def myCallbackRoutine(cap, action):
     global handled
-    # only render the floor during GLRender actions:
     if not action.isOfType(coin.SoGLRenderAction.getClassTypeId()):
         return
     cap.stencilBuffer()
@@ -103,9 +103,8 @@ def myCallbackRoutine(cap, action):
 def main():
     app = QtGui.QApplication(sys.argv)
     viewer = quarter.QuarterWidget()
-
     # build a scene (sphere, cube)
-    plane = coin.SbPlane(coin.SbVec3f(0, 1, 1), coin.SbVec3f(0, 0, 0))
+    plane = coin.SbPlane(coin.SbVec3f(0, 0, 1), coin.SbVec3f(0, 0, 0))
     root = coin.SoSeparator()
     myCallback = coin.SoCallback()
     cap = CapPlane(plane, root)

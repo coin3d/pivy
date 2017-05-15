246b859a71bc14d9764f9180d996ffa0d40cc6e9
looooo <sppedflyer@gmail.com>
2017-05-15 20:58:48 +0200

    add minimal example for soqt wrapping problem

---
+++ examples/soqt/renderAreaCallback.py
@@ -0,0 +1,20 @@
+import sys
+from pivy import coin
+from pivy.gui import soqt
+from PySide.QtCore import QEvent
+
+def foo(a, event):
+	if event.type() == QEvent.MouseButtonPress:
+		print(event)  # event is not a QMouseEvent like it was with pyqt4
+		print(event.button())
+
+appWindow = soqt.SoQt.init(sys.argv[0])
+root = coin.SoSeparator()
+myRenderArea = soqt.SoQtRenderArea(appWindow)
+myRenderArea.setSceneGraph(root)
+myRenderArea.setTitle("My Event Handler")
+myRenderArea.setEventCallback(foo, myRenderArea)
+
+myRenderArea.show()
+soqt.SoQt.show(appWindow)
+soqt.SoQt.mainLoop()

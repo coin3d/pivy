e294a156ddb647f0695bf99bfc3f1e4ac585eda9
looooo <sppedflyer@gmail.com>
2017-10-05 18:52:40 +0200

    some functions to visualize mesh

--- pivy/graphics/__init__.py
+++ pivy/graphics/__init__.py
@@ -1,51 +1,8 @@
 from pivy import coin
 from pivy.utils import getPointOnScreen
-import numpy as np
-
-
-COLORS = {
-    "black":  (0., 0., 0.),
-    "white":  (1., 1., 1.),
-    "grey":   (.5, .5, .5),
-    "red":    (1., 0., 0.),
-    "blue":   (0., 0., 1.),
-    "green":  (0., 1., 1.),
-    "yellow": (0., 1., 0.)
-}
-
-def simple_quad_mesh(points, num_u, num_v, colors=None):
-    msh = coin.SoQuadMesh()
-    vertexproperty = coin.SoVertexProperty()
-    vertexproperty.vertex.setValues(0, len(points), points)
-    msh.verticesPerRow = num_u
-    msh.verticesPerColumn = num_v
-    if colors:
-        vertexproperty.materialBinding = coin.SoMaterialBinding.PER_VERTEX
-        for i in range(len(colors)):
-            vertexproperty.orderedRGBA.set1Value(i, coin.SbColor(colors[i]).getPackedValue())
-    msh.vertexProperty = vertexproperty
-    msh
-
-def simple_poly_mesh(verts, poly, color=None):
-    color = color or COLORS["grey"]
-    _vertices = [list(v) for v in verts]
-    _polygons = []
-    for pol in poly:
-        _polygons += list(pol) + [-1]
-    sep = coin.SoSeparator()
-    vertex_property = coin.SoVertexProperty()
-    face_set = coin.SoIndexedFaceSet()
-    shape_hint = coin.SoShapeHints()
-    shape_hint.vertexOrdering = coin.SoShapeHints.COUNTERCLOCKWISE
-    shape_hint.creaseAngle = np.pi / 3
-    face_mat = coin.SoMaterial()
-    face_mat.diffuseColor = color
-    vertex_property.vertex.setValues(0, len(_vertices), _vertices)
-    face_set.coordIndex.setValues(0, len(_polygons), list(_polygons))
-    vertex_property.materialBinding = coin.SoMaterialBinding.PER_VERTEX_INDEXED
-    sep += [shape_hint, vertex_property, face_mat, face_set]
-    return sep
-
+from .colors import COLORS
+from .plot import plot
+from .mesh import simple_quad_mesh, simple_poly_mesh
 
 
 class Object3D(coin.SoSeparator):
---
+++ pivy/graphics/viewer.py
@@ -0,0 +1,42 @@
+from pivy import quarter, coin
+from PySide import QtGui, QtCore
+import tempfile
+
+class Viewer(quarter.QuarterWidget):
+    def __init__(self, *args, **kwrds):
+        try:
+            self.app = QtGui.QApplication([])
+        except RuntimeError:
+            self.app = QtGui.QApplication.instance()
+
+        super(Viewer, self).__init__(*args, **kwrds)
+        self.sg = coin.SoSeparator()
+        self.sg += [coin.SoOrthographicCamera()]
+        self.setSceneGraph(self.sg)
+        self.setBackgroundColor(coin.SbColor(1,1,1))
+        self.setWindowFlags(self.windowFlags() | QtCore.Qt.WindowStaysOnTopHint)
+
+    def show(self, exec_widget=True):
+        super(Viewer, self).show()
+        self.viewAll()
+        rec = self.app.desktop().screenGeometry()
+        self.move(rec.width() - self.size().width(), 
+                  rec.height() - self.size().height())
+        if not exec_widget:
+            timer = QtCore.QTimer()
+            # timer.timeout.connect(self.close)
+            timer.singleShot(20, self.close)
+        self.app.exec_()
+        try:
+            from IPython.display import Image
+            return Image(self.name)
+        except ImportError as e:
+            print(e)
+
+
+
+    def closeEvent(self, *args):
+        image = self.grabFrameBuffer()
+        _, name = tempfile.mkstemp(suffix=".png")
+        image.save(name, "png")
+        self.name = name

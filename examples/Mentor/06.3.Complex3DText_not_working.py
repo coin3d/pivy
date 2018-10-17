f9d88fc092995ba077054d4e73069be2f266f71b
looooo <sppedflyer@gmail.com>
2018-10-17 10:05:10 +0200

    example Complex3dText: add license and shebang this was unintentional removed at some point

--- examples/Mentor/06.3.Complex3DText_not_working.py
+++ examples/Mentor/06.3.Complex3DText_not_working.py
@@ -1,3 +1,27 @@
+#!/usr/bin/env python
+###
+# Copyright (c) 2002-2007 Systems in Motion
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+###
+# This is an example from the Inventor Mentor,
+# chapter 6, example 3.
+#
+# This example renders arguments as text within an
+# SoXTExaminerViewer.  It is a little fancier than 6.2.
+#
+
 """
 This is an example from the Inventor Mentor,
 chapter 6, example 3.

8b3bebb3bfd68bc9b01f94860b2f596ca519f9ab
lorenz <looooo@users.noreply.github.com>
2019-10-21 12:23:56 +0200

    fix travis build

--- packaging/conda/build.sh
+++ packaging/conda/build.sh
@@ -1,2 +1,9 @@
+if [[ ${HOST} =~ .*linux.* ]]; then
+  sed -i 's|_qt5gui_find_extra_libs(EGL.*)|_qt5gui_find_extra_libs(EGL "EGL" "" "")|g' $PREFIX/lib/cmake/Qt5Gui/Qt5GuiConfigExtras.cmake
+  sed -i 's|_qt5gui_find_extra_libs(OPENGL.*)|_qt5gui_find_extra_libs(OPENGL "GL" "" "")|g' $PREFIX/lib/cmake/Qt5Gui/Qt5GuiConfigExtras.cmake
+fi
+
+PIVY_CPP_FLAGS='-std=c++1z '
+
 $PYTHON setup.py clean
 $PYTHON setup.py install

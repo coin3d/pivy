e837407c5df83b58c114ead2de32c449a7dced82
looooo <sppedflyer@gmail.com>
2022-04-29 12:54:54 +0200

    conda: set Python_EXECUTABLE

--- packaging/conda/build.sh
+++ packaging/conda/build.sh
@@ -11,6 +11,7 @@ PIVY_CPP_FLAGS='-std=c++1z '
 cmake -G "Ninja" \
     -D CMAKE_BUILD_TYPE="Release" \
     -D CMAKE_INSTALL_PREFIX:FILEPATH=$PREFIX \
+    -D Python_EXECUTABLE:FILEPATH=$PREFIX/bin/python \
     ..
 
 ninja install

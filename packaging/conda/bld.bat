3a344e170397f893f0dd49004552c0326a557328
looooo <sppedflyer@gmail.com>
2022-04-29 12:54:54 +0200

    conda: set Python_EXECUTABLE

--- packaging/conda/bld.bat
+++ packaging/conda/bld.bat
@@ -4,6 +4,7 @@ cd build
 cmake -G "Ninja" ^
     -D CMAKE_BUILD_TYPE="Release" ^
     -D CMAKE_INSTALL_PREFIX:FILEPATH=%LIBRARY_PREFIX% ^
+    -D Python_EXECUTABLE:FILEPATH=%PREFIX%/python ^
     ..
 
 
